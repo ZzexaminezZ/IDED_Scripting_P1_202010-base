ead2ef1058498ee56350e69cb72e6ed7018d52ba
jhony.m <iiexamineii@gmail.com>
2020-03-10 18:03:09 -0500

    Add files via upload

--- IDED_Scripting_P1_202010-base/Program.cs
+++ IDED_Scripting_P1_202010-base/Program.cs
@@ -4,12 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IDED_Scripting_P1_202010_base
+
+namespace IDED_Scripting_P1_202010_base.Logic
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            Prop cofre = new Prop(EPropType.Chest);
+            Human julian = new Human( EUnitClass.Ranger , 111123, 12323123, 112312323, 1023010,9999 );
+            Unit anna = new Unit(EUnitClass.Orc, 111123, 12323123, 112312323, 10001);
+
+          Console.WriteLine( julian.Interact(anna));
+
+            Console.WriteLine(julian.Interact(cofre));
+            Console.WriteLine( julian.UnitClass);
+            Console.WriteLine(julian.Attack);
+            Console.WriteLine(julian.Defense);
+            Console.WriteLine(julian.Speed);
+            Console.WriteLine(julian.AtkRange);
+            Console.WriteLine(julian.potential);
+            Console.WriteLine(julian.MoveRange);
+            Position mover = new Position(5, 3);
+            Console.WriteLine("Posicion en x {0}, posicion en y {1}", julian.CurrentPosition.x, julian.CurrentPosition.y);
+            Console.WriteLine(julian.Move(mover));
+            Console.WriteLine("Posicion en x {0}, posicion en y {1}", julian.CurrentPosition.x , julian.CurrentPosition.y);
+
+           Console.WriteLine(julian.ChangeClass(EUnitClass.Mage));
+
+
+
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
