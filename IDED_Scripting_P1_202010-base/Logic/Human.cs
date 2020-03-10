253f9848a5c6e0985a78caed765c1491dda4d567
jhony.m <iiexamineii@gmail.com>
2020-03-10 17:59:32 -0500

    Version final
    Este es el parcial totalmente funcional

--- IDED_Scripting_P1_202010-base/Logic/Human.cs
+++ IDED_Scripting_P1_202010-base/Logic/Human.cs
@@ -2,17 +2,94 @@
 {
     public class Human : Unit
     {
-        public float Potential { get; set; }
+        private float Potential;
+        public float potential { get => Potential ;
+            set { Potential = value;} }
 
         public Human(EUnitClass _unitClass, int _atk, int _def, int _spd, int _moveRange, float _potential)
             : base(_unitClass, _atk, _def, _spd, _moveRange)
         {
+            UniqueHumanClass();                    
             Potential = _potential;
+            StatsSetter();
+            SetStats();
+            MaxPotential();
         }
 
+        private void MaxPotential()
+        {
+            if (Potential > 10)
+            {
+                Potential = 10;
+            }
+
+        }
+        private void SetStats()
+        {
+            Attack = (Attack +Attack* (potential / 100));
+            Defense = (Defense +Defense * (potential / 100));
+
+            if (Attack > 255)
+            {
+                Attack = 255;
+            }
+            if (Defense > 255)
+            {
+                Defense = 255;
+            }
+        }
+        private void UniqueHumanClass()
+        {
+            switch (UnitClass)
+            {
+                case EUnitClass.Villager:
+                    break;
+                case EUnitClass.Squire:
+                    break;
+                case EUnitClass.Soldier:
+                    break;
+                case EUnitClass.Ranger:
+                    break;
+                case EUnitClass.Mage:
+                    break;
+                case EUnitClass.Imp:
+                    UnitClass = 0;
+
+                    break;
+                case EUnitClass.Orc:
+                    UnitClass = 0;
+                    break;
+                case EUnitClass.Dragon:
+                    UnitClass = 0;
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
         public virtual bool ChangeClass(EUnitClass newClass)
         {
-            return false;
+            bool cambiar = false;
+
+            if (UnitClass == EUnitClass.Squire && newClass == EUnitClass.Soldier)
+            {
+                cambiar = true;
+            }
+            if (UnitClass == EUnitClass.Soldier && newClass == EUnitClass.Squire)
+            {
+                cambiar = true;
+            }
+            if (UnitClass == EUnitClass.Mage && newClass == EUnitClass.Ranger)
+            {
+                cambiar = true;
+            }
+            if (UnitClass == EUnitClass.Ranger && newClass == EUnitClass.Mage)
+            {
+                cambiar = true;
+            }
+
+            return cambiar;
         }
     }
 }
--- IDED_Scripting_P1_202010-base/Logic/Unit.cs
+++ IDED_Scripting_P1_202010-base/Logic/Unit.cs
@@ -1,42 +1,295 @@
-﻿namespace IDED_Scripting_P1_202010_base.Logic
+﻿using System;
+
+
+namespace IDED_Scripting_P1_202010_base.Logic
 {
     public class Unit
     {
-        public int BaseAtk { get; protected set; }
-        public int BaseDef { get; protected set; }
-        public int BaseSpd { get; protected set; }
+         private int baseAtk;
+         private int baseDef;
+         private int baseSpd;
+         private int moveRange;
+        public int BaseAtk { get => baseAtk ;
 
-        public int MoveRange { get; protected set; }
+             protected set { 
+                if (value < 255)
+                {
+                    baseAtk = value;
+                }
+                else
+                {
+                    if (value >= 255)
+                    {
+                        baseAtk = 255;
+                    }
+                }
+                           
+             }
+        }
+        public int BaseDef { get => baseDef;
+            protected set{
+                if (value < 255)
+                {
+                    baseDef = value;
+                }
+                else
+                {
+                    if (value >= 255)
+                    {
+                        baseDef = 255;
+                    }
+                }
+
+            }
+        }
+        public int BaseSpd { get => baseSpd;
+            protected set
+            {
+                if (value < 255)
+                {
+                    baseSpd = value;
+                }
+                else
+                {
+                    if (value >= 255)
+                    {
+                        baseSpd = 255;
+                    }
+                }
+            }
+        }
+
+        public int MoveRange { get => moveRange;
+            protected set
+            {
+                moveRange = value;   
+            }
+        }
         public int AtkRange { get; protected set; }
 
         public float BaseAtkAdd { get; protected set; }
         public float BaseDefAdd { get; protected set; }
         public float BaseSpdAdd { get; protected set; }
 
-        public float Attack { get; }
-        public float Defense { get; }
-        public float Speed { get; }
+        public float Attack { get; protected set; }
+        public float Defense { get; protected set; }
+        public float Speed { get; protected set; }
 
-        protected Position CurrentPosition;
+        public Position CurrentPosition;
 
         public EUnitClass UnitClass { get; protected set; }
+     
 
         public Unit(EUnitClass _unitClass, int _atk, int _def, int _spd, int _moveRange)
         {
+
+
+
+            //Lo ideal para probar este metodo de movimiento es poner estos X y Y como numero conocidos, para compararlos con posiciones 
+            //Dentro del
+            int x =new Random().Next();
+            int y = new Random().Next();
+            Position StartPosition = new Position(x, y);
+            CurrentPosition = StartPosition;
+
+
             UnitClass = _unitClass;
-            BaseAtk = _atk;
-            BaseDef = _def;
-            BaseSpd = _spd;
-            MoveRange = _moveRange;
+            baseAtk = _atk;
+            baseDef = _def;
+            baseSpd = _spd;
+            moveRange = _moveRange;          
+            StatsSetter();
+            MaxMovRang();
+            
         }
 
+        private void MaxMovRang()
+        {
+            if (moveRange <0)
+            {
+                moveRange = 1;
+
+            }
+            if (moveRange > 10)
+            {
+                moveRange = 10;
+            }
+
+        }
         public virtual bool Interact(Unit otherUnit)
         {
-            return false;
+
+            bool interactuar = true;
+            switch (UnitClass)
+            {
+                case EUnitClass.Villager:
+                    interactuar = false;
+                    break;
+                case EUnitClass.Squire:
+                    if (otherUnit.UnitClass == EUnitClass.Villager)
+                    {
+                        interactuar =  false;
+                    }
+                    break;
+                case EUnitClass.Soldier:
+                    if (otherUnit.UnitClass == EUnitClass.Villager)
+                    {
+                        interactuar = false;
+                    }
+                    break;
+                case EUnitClass.Ranger:
+                    if (otherUnit.UnitClass == EUnitClass.Ranger || otherUnit.UnitClass == EUnitClass.Dragon)
+                    {
+                        interactuar = false;
+                    }
+                    break;
+                case EUnitClass.Mage:
+                    if (otherUnit.UnitClass == EUnitClass.Mage)
+                    {
+                        interactuar = false;
+                    }
+                    break;
+                case EUnitClass.Imp:
+                    if (otherUnit.UnitClass == EUnitClass.Dragon)
+                    {
+                        interactuar = false;
+                    }
+                    break;
+                case EUnitClass.Orc:
+                    if (otherUnit.UnitClass == EUnitClass.Dragon)
+                    {
+                        interactuar = false;
+                    }
+                    break;
+                case EUnitClass.Dragon:
+                    break;
+                default:
+                    break;
+            }
+
+            return interactuar;
         }
 
-        public virtual bool Interact(Prop prop) => false;
+        public virtual bool Interact(Prop prop)
+        {
+            if (UnitClass!= EUnitClass.Villager)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
 
-        public bool Move(Position targetPosition) => false;
+        public bool Move(Position targetPosition)
+        {
+            bool movimiento = false;
+
+            int x = CurrentPosition.x - targetPosition.x;
+            int y = CurrentPosition.y - targetPosition.y;
+
+            if ((x >= -(moveRange) && x <= moveRange) && (y >= -(moveRange) && y <= moveRange))
+            {
+                movimiento = true;
+                CurrentPosition = targetPosition;
+            }
+            else
+            {
+                movimiento = false;
+            }
+
+
+
+
+            return movimiento;
+        }
+
+
+        protected void StatsSetter ()
+        {
+           
+
+            switch (UnitClass)
+            {
+                case EUnitClass.Villager:
+                    BaseAtkAdd = 0.0f;
+                    BaseDefAdd = 0.0F;
+                    BaseSpdAdd = 0.0F;
+                    AtkRange = 0;
+                    break;
+                case EUnitClass.Squire:
+                    BaseAtkAdd = 0.02F;
+                    BaseDefAdd = 0.01F;
+                    BaseSpdAdd = 0.00F;
+                    AtkRange = 1;
+                    break;
+                case EUnitClass.Soldier:
+                    BaseAtkAdd = 0.03F;
+                    BaseDefAdd = 0.02F;
+                    BaseSpdAdd = 0.01F;
+                    AtkRange = 1;
+                    break;
+                case EUnitClass.Ranger:
+                    BaseAtkAdd = 0.01F;
+                    BaseDefAdd = 0.00F;
+                    BaseSpdAdd = 0.03F;
+                    AtkRange = 3;
+                    break;
+                case EUnitClass.Mage:
+                    BaseAtkAdd = 0.03F;
+                    BaseDefAdd = 0.01F;
+                    BaseSpdAdd = -0.01F;
+                    AtkRange = 3;
+                    break;
+                case EUnitClass.Imp:
+                    BaseAtkAdd = 0.01F;
+                    BaseDefAdd = 0.00F;
+                    BaseSpdAdd = 0.00F;
+                    AtkRange = 1;
+                    break;
+                case EUnitClass.Orc:
+                    BaseAtkAdd = 0.04F;
+                    BaseDefAdd = 0.02F;
+                    BaseSpdAdd = -0.02F;
+                    AtkRange = 1;
+                    break;
+                case EUnitClass.Dragon:
+                    BaseAtkAdd = 0.5F;
+                    BaseDefAdd = 0.3F;
+                    BaseSpdAdd = 0.2F;
+                    AtkRange = 5;
+                    break;
+                default:
+                    break;
+            }
+
+            if (UnitClass != EUnitClass.Villager)
+            {
+                Attack = (baseAtk + (baseAtk * BaseAtkAdd));
+                if (Attack > 255)
+                {
+                    Attack = 255;
+                }
+                Defense = (baseDef + (baseDef * BaseDefAdd));
+                if (Defense > 255)
+                {
+                    Defense = 255;
+                }              
+            }
+            else
+            {
+                Attack = 0;
+                Defense = 0;
+                AtkRange = 0;
+            }
+            Speed = (baseSpd + (baseSpd * BaseSpdAdd));
+            if (Speed > 255)
+            {
+                Speed = 255;
+            }
+
+
+        }
     }
 }
